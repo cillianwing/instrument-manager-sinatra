36c361972ef05679f706ed796ddf01a4f37bbc99
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-06-16 00:28:39 -0500

    Add repair index page and route

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -4,4 +4,41 @@ require 'rack-flash'
 class RepairsController < ApplicationController
   use Rack::Flash
 
+  get '/repairs' do
+    if logged_in?
+      @repairs = Repair.all
+      erb :'/repairs/index'
+    else
+      '/login'
+    end
+  end
+
+  get '/repairs/news' do
+
+  end
+
+  post '/repairs' do
+
+  end
+
+  get '/repairs/:id' do
+
+  end
+
+  get '/repairs/:id/edit' do
+
+  end
+
+  patch '/repairs/:id' do
+
+  end
+
+  get '/repairs/:id/complete' do
+
+  end
+
+  delete '/repairs/:id' do
+    
+  end
+
 end
