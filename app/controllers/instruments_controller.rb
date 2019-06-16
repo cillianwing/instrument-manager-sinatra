94bc578d4a89154f5abfa5347bee86cd9b29c8ed
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-06-15 19:26:12 -0500

    Add create instrument functionality

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,37 +1,79 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class InstrumentsController < ApplicationController
+  use Rack::Flash
 
-  # GET: /instruments
-  get "/instruments" do
-    erb :"/instruments/index.html"
+  get 'instruments' do
+    if logged_in?
+      @user = User.find_by_id(session[:user_id])
+      @instruments = @user.instruments
+      erb :'/instruments/index'
+    else
+      redirect '/login'
+    end
   end
 
-  # GET: /instruments/new
-  get "/instruments/new" do
-    erb :"/instruments/new.html"
+  get '/new_instrument' do
+    @statuses = ["Usable", "Needs Repair", "In Repair"]
+    if logged_in?
+      erb :'/instruments/new'
+    else
+      redirect '/login'
+    end
   end
 
-  # POST: /instruments
-  post "/instruments" do
-    redirect "/instruments"
+  post '/instruments' do
+    @instrument = Instrument.create(params)
+    @instrument.user_id = session[:user_id]
+    redirect "/instruments/#{@instrument.id}"
   end
 
-  # GET: /instruments/5
-  get "/instruments/:id" do
-    erb :"/instruments/show.html"
+  get '/instruments/:id' do
+    if logged_in?
+      @user = User.find_by_id(session[:user_id])
+      @instrument = Instrument.find_by(user_id: @user.id)
+      erb :'/instruments/show'
+    else
+      redirect '/login'
+    end
   end
 
-  # GET: /instruments/5/edit
-  get "/instruments/:id/edit" do
-    erb :"/instruments/edit.html"
-  end
-
-  # PATCH: /instruments/5
-  patch "/instruments/:id" do
-    redirect "/instruments/:id"
-  end
-
-  # DELETE: /instruments/5/delete
-  delete "/instruments/:id/delete" do
-    redirect "/instruments"
-  end
 end
+
+=begin
+# GET: /instruments
+get "/instruments" do
+  erb :"/instruments/index.html"
+end
+
+# GET: /instruments/new
+get "/instruments/new" do
+  erb :"/instruments/new.html"
+end
+
+# POST: /instruments
+post "/instruments" do
+  redirect "/instruments"
+end
+
+# GET: /instruments/5
+get "/instruments/:id" do
+  erb :"/instruments/show.html"
+end
+
+# GET: /instruments/5/edit
+get "/instruments/:id/edit" do
+  erb :"/instruments/edit.html"
+end
+
+# PATCH: /instruments/5
+patch "/instruments/:id" do
+  redirect "/instruments/:id"
+end
+
+# DELETE: /instruments/5/delete
+delete "/instruments/:id/delete" do
+  redirect "/instruments"
+end
+=end
--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,12 @@ ActiveRecord::Schema.define(version: 20190615231221) do
   create_table "accessories", force: :cascade do |t|
     t.string   "make"
     t.string   "model"
-    t.string   "status"
     t.string   "status_comments"
     t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "type_of"
+    t.string   "status"
   end
 
   create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
@@ -42,12 +42,12 @@ ActiveRecord::Schema.define(version: 20190615231221) do
   create_table "instruments", force: :cascade do |t|
     t.string   "make"
     t.string   "model"
-    t.string   "status"
     t.string   "status_comments"
     t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "type_of"
+    t.string   "status"
   end
 
   create_table "users", force: :cascade do |t|
